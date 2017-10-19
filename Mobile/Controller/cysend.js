830730acea806890c34608f4d4ebc0a930831681
SadiaY <mcsf14m002@pucit.edu.pk>
2017-10-20 00:29:27 +0500

    SDsds

--- Mobile/Controller/cysend.js
+++ Mobile/Controller/cysend.js
@@ -4,14 +4,14 @@
 var appendQuery = require('append-query')
 const md5 = require('md5');
 const request = require('request')
+const cysend = require('../../cysend')
 
 exports.checkBalance =  function (req, res) {
-    const api_username = "tW12YxFFGHmzx8vQjG5ZKwPRQTSQuFYK"
-    let api_url ="https://honeypot.cysend.ch/merchant_api/4.1"
+    let api_url =cysend.api_url
 
     const params = {
         'function': 'get_balance',
-        'username': api_username,
+        'username': cysend.api_username,
         'format': 'json'
     }
 
@@ -19,11 +19,11 @@ exports.checkBalance =  function (req, res) {
     Object.keys(params).forEach((key)=>{
         hash += params[key] + "|"
     })
-    hash += "HZaZzBjzHMrJWgjjZFnkz2XaneHh8w3EUXWjqecCg6Lv6qgSiy1AkB3SByD62UdD";
+    hash += cysend.api_password;
     console.log('before *********', hash)
     params['hash'] = md5(hash)
     // console.log('params', params)
-    headers = {'Content-Type': 'application/x-www-form-urlencoded', 'Content-Language': 'en-US'}
+    headers = cysend.api_headers
     request({url: api_url, method: 'POST',form:params, headers: headers}, function (err, http,body) {
         res.status(200).json({ 'http' :http})
     })
---
+++ cysend.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Afraz on 10/20/2017.
+ */
+
+
+module.exports = {
+    'api_username':  'tW12YxFFGHmzx8vQjG5ZKwPRQTSQuFYK',
+    'api_url':"https://honeypot.cysend.ch/merchant_api/4.1",
+    'api_password': 'HZaZzBjzHMrJWgjjZFnkz2XaneHh8w3EUXWjqecCg6Lv6qgSiy1AkB3SByD62UdD',
+    'api_headers' : {'Content-Type': 'application/x-www-form-urlencoded', 'Content-Language': 'en-US'}
+}
