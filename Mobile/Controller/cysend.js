790d4989812039d1704eb87b6fdc1d99107079ef
SadiaY <mcsf14m002@pucit.edu.pk>
2017-10-19 23:50:32 +0500

    Ddsds

--- Mobile/Controller/cysend.js
+++ Mobile/Controller/cysend.js
@@ -23,7 +23,7 @@ exports.checkBalance =  function (req, res) {
     params['hash'] = md5(hash)
     console.log('params', params)
     headers = {'Content-Type': 'application/x-www-form-urlencoded', 'Content-Language': 'en-US'}
-    request({url: api_url, form:params,qs:params, headers: headers}, function (err, http,body) {
+    request({url: api_url,qs:params, headers: headers}, function (err, http,body) {
         console.log(err, http, body)
         res.status(200).json({'err': err, 'http' :http,'body': body})
     })
